63791cbe80992c7cfa88dd59fa7095b9619e2ef7
Анна <anna@MacBook-Air-Anna.local>
2026-07-24 01:11:59 +0300

    Improve reviews section

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -8,11 +8,23 @@ function Reviews() {
       <div className="container">
         <h2 className="reviews__title">What Our Customers Say</h2>
 
-        <div className="reviews__rating">
-          <span className="stars">★★★★★</span>
-          <p className="reviews__rate-text text-sm">
-            5.0 / 5.0 rating on Google
-          </p>
+        <div className="reviews__google">
+          <div className="reviews__google-left">
+            <span className="reviews__google-stars">★★★★★</span>
+
+            <p className="reviews__google-text">
+              <strong>5.0/ 5.0</strong>
+            </p>
+
+            <img
+              className="reviews__google-logo"
+              src="/icons/google.svg"
+              alt="Google"
+              loading="lazy"
+            />
+          </div>
+
+          <div className="reviews__google-button">Review us on Google</div>
         </div>
 
         <div className="reviews__cards">
